6d33ded42ca1aac559f558ae9bc759c37432ab90
Uday <us007sep@gmail.com>
2022-07-18 03:29:06 +0530

    Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,12 +5,15 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Schema } from "./AppState";
-import { useContext} from "react";
-import { Item } from "./ItemReducer";
+import { useContext, useEffect} from "react";
+import { finished_writing, Item, started_writing } from "./ItemReducer";
 import { UserContext } from "./UserContext";
 
+interface Iitems{
+    data:{items:Item[]}
+}
 
 const useStyle = makeStyles({
     navbar:{
@@ -47,15 +50,32 @@ export default function Navigation(){
     const context = useContext(UserContext);
 
     const userExist = context;
+    const dispatch = useDispatch();
+    const selector = useSelector((x:Schema) => x.ItemsReducer);
 
-    var list : Item[] = [];
-    const selector = useSelector((x:Schema) => x.ItemsReducer)
+    useEffect(()=>{
+        async function api() {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
 
-    const inputEvent = (e:string) =>{
-        list = [];
-        selector.it.filter(x=> x.name.toLowerCase().includes(e)).map((x)=>list.push(x));
-        console.log(list);
-    }
+            var graphql = JSON.stringify({
+            query: "{\n  items{\n    name\n    description\n    featured_image1\n    featured_image2\n    featured_image3\n    featured_image4\n    category\n    price\n    home\n    new\n  }\n}",
+            variables: {}
+            })
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: graphql,
+            };
+
+            const response = await fetch("https://houseofindya.herokuapp.com/graphql", requestOptions);
+            const json :Iitems= await response.json();
+
+            dispatch(finished_writing(json.data.items));
+        }
+        dispatch(started_writing);
+        api();    
+    },[dispatch])
 
     return(
         <>
@@ -88,7 +108,6 @@ export default function Navigation(){
                     placeholder="Search"
                     className="me-2"
                     aria-label="Search"
-                    onChange={(e) => inputEvent(e.target.value)}
                     />
                     <Button variant="outlined" style={{backgroundColor:'#b69575', color:'#fff'}}>Search</Button>
                 </Form>
